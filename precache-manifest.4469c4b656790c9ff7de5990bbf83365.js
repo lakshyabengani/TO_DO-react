00ed50d8c2d784097de031ad14adcb8f2c94f1e8
Lakshya Bengani <lakshyabegani14@gmail.com>
2020-04-19 11:51:08 +0530

    Updates

---
+++ precache-manifest.4469c4b656790c9ff7de5990bbf83365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c8469226cf10610f592dec0ac9a0a6",
+    "url": "/TO_DO-react/index.html"
+  },
+  {
+    "revision": "748b7a889bc4e0ec8016",
+    "url": "/TO_DO-react/static/css/main.781e2d05.chunk.css"
+  },
+  {
+    "revision": "8f44147a2766178cf105",
+    "url": "/TO_DO-react/static/js/2.c164d54b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TO_DO-react/static/js/2.c164d54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748b7a889bc4e0ec8016",
+    "url": "/TO_DO-react/static/js/main.579a1cf3.chunk.js"
+  },
+  {
+    "revision": "b84569e59d105c0088bf",
+    "url": "/TO_DO-react/static/js/runtime-main.21511c07.js"
+  }
+]);
